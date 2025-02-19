d2cfca86c44416e25a493dd1f18f808200e5b197
Ed Cabiltes <edcabiltes@outlook.com>
2025-02-19 17:20:11 +0800

    improve user feedback with loading state and error handling

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,36 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch(`http://localhost:8000/blogs`)
+    setTimeout(() => {
+      fetch(`http://localhost:8000/blogs`)
       .then(res => {
+        if (!res.ok) {
+          throw Error(`could not fetch the data for that resource`)
+        }
         return res.json()
       })
       .then(data => {
+        setIsPending(false);
         setBlogs(data)
+        setError(null)
       })
+      .catch(err => {
+        setIsPending(false)
+        setError(err.message)
+      })
+    }, 1000)
+
   }, [])
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} />}
+      { error && <div>{ error }</div>}
+      { isPending && <div>Loading...</div>}
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   )
 }
